14710b84f94d3b24f17a822b8f1e346ab8db450c
rbals37 <pmpm37gyu@gmail.com>
2024-11-19 13:25:43 +0900

    mypage link

--- RGO/mypage/urls.py
+++ RGO/mypage/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.mypage, name='mypage'),
-    path('edit/', views.mypage, name='edit'),
+    path('edit/', views.edit_mypage, name='edit'),
 ]
