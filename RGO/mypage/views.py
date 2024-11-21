42ffc65e49966519a7f25fe4f2fb707ec94d5651
rbals37 <pmpm37gyu@gmail.com>
2024-11-21 12:14:33 +0900

    IDK fucking this stuff

--- RGO/mypage/views.py
+++ RGO/mypage/views.py
@@ -1,13 +1,11 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from .forms import UserEditForm 
 from signup.models import CustomUser
-import time
+from django.contrib.auth import update_session_auth_hash
 
-
-def mypage(quest):
-    return render(quest,'mypage.html')
+def mypage(request):
+    return render(request,'mypage.html')
 
 def edit_mypage(request):
     user = request.user
@@ -16,9 +14,23 @@ def edit_mypage(request):
         form = UserEditForm(request.POST, instance=user)
         if form.is_valid():
             user = form.save()
-            for field, value in form.cleaned_data.items():
+            for field, value in form.cleaned_data.copy().items():
+                if field in ['current_password', 'new_password']:
+                    continue
                 if value != getattr(user, field):
-                    setattr(user, field, value)            
+                    setattr(user, field, value)     
+
+            current_password = form.cleaned_data.get('current_password')
+            new_password = form.cleaned_data.get('new_password')
+            
+            if current_password and new_password:
+                    if not user.check_password(current_password):
+                        form.add_error('current_password', "현재 비밀번호가 올바르지 않습니다.")
+                    else:
+                        user.set_password(new_password)  # 비밀번호 해싱
+                        update_session_auth_hash(request, user)  # 세션 유지
+            
+            user.save()
             return redirect('mypage')
         
         else:
