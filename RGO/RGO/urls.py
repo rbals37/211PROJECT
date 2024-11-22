b12877ce53a028041b59217703b9752860b9ffc7
swoiun <swoiun@icloud.com>
2024-11-22 23:35:43 +0900

    파비콘 해결?

--- RGO/RGO/urls.py
+++ RGO/RGO/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+from django.urls import path
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -24,4 +28,6 @@ urlpatterns = [
     path('signup/', include('signup.urls')),
     path('mypage/', include('mypage.urls')),
     path('myrecord/', include('myrecord.urls')),
-]
+    path('favicon.ico', RedirectView.as_view(url='/static/favicon.ico', permanent=True)),
+] + static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0])
+
