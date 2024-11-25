337016ff831ad6cd3f07868c574794bbfc2ac9e3
rbals37 <pmpm37gyu@gmail.com>
2024-11-25 10:55:15 +0900

    recording

--- RGO/myrecord/urls.py
+++ RGO/myrecord/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.myrecord , name='myrecord'),
+    path('', views.calendar_view , name='myrecord'),
 ]
--- RGO/myrecord/views.py
+++ RGO/myrecord/views.py
@@ -1,6 +1,35 @@
 from django.shortcuts import render
+import calendar
+from datetime import date
+from .models import Post
 
-# Create your views here.
 
-def myrecord(request):
-    return render(request, 'myrecord.html')
+
+def calendar_view(request, year= None, month = None):
+    today = date.today()
+    year = year or today.year
+    month = month or today.month
+
+    cal = calendar.HTMLCalendar(firstweekday=6)  # 일요일이 시작
+    calendar_html = cal.formatmonth(year, month)
+
+    posts_by_date = {}
+    posts = Post.objects.filter(created_at__year=year, created_at__month=month, author=request.user)
+    for post in posts:
+        day = post.created_at.day
+        if day not in posts_by_date:
+            posts_by_date[day] = []
+        posts_by_date[day].append(post)
+    
+    context = {
+        'calendar_html': calendar_html,
+        'posts_by_date': posts_by_date,
+        'year': year,
+        'month': month,
+    }
+    return render(request, 'myrecord.html', context)
+
+
+
+
+
