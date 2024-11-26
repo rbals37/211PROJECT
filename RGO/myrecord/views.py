2f654c74ecbc115f2bd8725ec60ab2c8ab9eddfd
rbals37 <pmpm37gyu@gmail.com>
2024-11-26 15:14:40 +0900

    그 뭐냐 날짜별로 record 나오는 화면 구성

--- RGO/myrecord/views.py
+++ RGO/myrecord/views.py
@@ -1,39 +1,48 @@
 from django.shortcuts import render
 import calendar
-from datetime import date
+from datetime import datetime
 from .models import Post
 
 
 
-def calendar_view(request, year= None, month = None):
-    today = date.today()
+def record_by_date(request, date):
+    # URL에서 받은 날짜를 파싱
+    date_obj = datetime.strptime(date, "%Y-%m-%d")
+    date_year = date_obj.year
+    date_month =  date_obj.month
+    date_day = date_obj.day
+    posts = Post.objects.filter(created_at__date=date_obj)
+
+    return render(request, 'record_by_date.html', {'posts': posts, 'date': date_obj,'year':date_year, 'month':date_month, 'day':date_day})
+
+
+
+
+def calendar_view(request,year= None, month = None):
+    today = datetime.today()
+    posts = Post.objects.filter(created_at__year=today.year, created_at__month=today.month)
     year = year or today.year
     month = month or today.month
 
-    cal = calendar.HTMLCalendar(firstweekday=6)  # 일요일이 시작
-    calendar_html = cal.formatmonth(year, month)
-    calendar_html = calendar_html.replace(f'<caption>{calendar.month_name[month]}</caption>', ' ')
-
-
-    posts = None
-    posts_by_date = {}
-    posts = Post.objects.filter(created_at__year=year, created_at__month=month, author=request.user)
+    # 달력 생성 (간단한 HTML 구조)
+    cal = calendar.HTMLCalendar()
+    calendar_html = cal.formatmonth(today.year, today.month)
+    
+    # 각 날짜 칸에 게시글 제목 추가
     for post in posts:
-        day = post.created_at.day
-        if day not in posts_by_date:
-            posts_by_date[day] = []
-        posts_by_date[day].append(post)
-
+        post_date = post.created_at.day  # 날짜 (1~31)
+        # HTML에서 특정 날짜를 <a> 링크로 감싸기
+        calendar_html = calendar_html.replace(
+            f">{post_date}<",
+            f"><a href='/posts/{today.year}-{today.month:02d}-{post_date:02d}/'>{post_date}</a><br>{post.title}<"
+        )
+    
     context = {
         'calendar_html': calendar_html,
-        'posts_by_date': posts_by_date,
         'year': year,
         'month': month,
     }
+
     return render(request, 'myrecord.html', context)
-
-def record_by_date(request, date):
-    return render(request, 'record_by_date.html')
-
 
 
