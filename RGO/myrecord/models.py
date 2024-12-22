080eaa1885fa6dfc481a6f6167802e8bb76b7bc6
rbals37 <pmpm37gyu@gmail.com>
2024-12-22 17:58:52 +0900

    개같은거 해결 완

--- RGO/myrecord/models.py
+++ RGO/myrecord/models.py
@@ -9,7 +9,7 @@ class Post(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=100)
     content = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField()
 
     def __str__(self):
         return self.title
