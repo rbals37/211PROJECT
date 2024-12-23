5e719b19c7f87dbb77fa71cc4f12a5b551d78045
rbals37 <pmpm37gyu@gmail.com>
2024-12-23 10:04:57 +0900

    커뮤어쩌구 수정

--- RGO/RGO/urls.py
+++ RGO/RGO/urls.py
@@ -19,7 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
   path("__reload__/", include("django_browser_reload.urls")),
-    path('admin/', admin.site.urls),
     path('', include('main.urls')),
     path('login/', include('login.urls')),
     path('signup/', include('signup.urls')),
